c49d1082bca90cefb8a3078a1a348ca9f581d811
Kaloyan <83769780+stoyanov-kaloyan@users.noreply.github.com>
2024-01-18 20:18:39 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,181 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import axios from "axios";
+import { useFonts } from "expo-font";
 
-export default function App() {
+const App = () => {
+  const [currentMonster, setCurrentMonster] = useState(null);
+  const [monsters, setMonsters] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchMonsters = async () => {
+    try {
+      const response = await axios.get("https://www.dnd5eapi.co/api/monsters");
+      setMonsters(response.data.results);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching monsters:", error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMonsters();
+  }, []);
+
+  const getRandomMonster = async () => {
+    if (monsters.length === 0) {
+      setCurrentMonster("No monsters available");
+      return;
+    }
+    const randomIndex = Math.floor(Math.random() * monsters.length);
+    const randomMonster = monsters[randomIndex];
+    console.log(randomMonster.url);
+    const url = "https://www.dnd5eapi.co" + randomMonster?.url;
+    try {
+      const response = await axios.get(url);
+      console.log(response.data);
+      setIsLoading(true);
+      // if (!response.data.image) {
+      //   getRandomMonster();
+      //   return;
+      // }
+      setCurrentMonster(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching monster:", error);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {isLoading && <Text>Loading...</Text>}
+      {!currentMonster?.image && currentMonster ? (
+        <Text
+          style={{
+            top: 80,
+            fontSize: 15,
+            fontWeight: "bold",
+            color: "white",
+          }}
+        >
+          Image not available
+        </Text>
+      ) : null}
+      <ImageBackground
+        source={
+          currentMonster
+            ? { uri: `https://www.dnd5eapi.co${currentMonster.image}` }
+            : null
+        }
+        style={styles.image}
+      >
+        <View
+          style={{
+            width: "80%",
+            height: "50%",
+            backgroundColor: "rgba(0,0,0,0.5)",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 10,
+            padding: 10,
+            opacity: 0.8,
+            shadowColor: "#000",
+            shadowOffset: {
+              width: 2,
+              height: 2,
+            },
+          }}
+        >
+          <Text style={styles.title}>
+            {currentMonster
+              ? currentMonster.name
+              : "Welcome to random monster generator!"}
+          </Text>
+          <Text style={styles.description}>
+            {currentMonster ? currentMonster.size : null}
+          </Text>
+          <Text style={styles.description}>
+            {currentMonster ? currentMonster.type : null}
+          </Text>
+          <Text style={styles.description}>
+            {currentMonster ? currentMonster.alignment : null}
+          </Text>
+          <Text style={styles.description}>
+            {currentMonster ? currentMonster.challenge_rating : null}
+          </Text>
+          <Text style={styles.description}>
+            {currentMonster ? currentMonster.hit_points : null}
+          </Text>
+          <Text style={styles.description}>
+            {currentMonster ? currentMonster.speed.walk : null}
+          </Text>
+        </View>
+      </ImageBackground>
+      <Pressable onPress={getRandomMonster}>
+        <Text style={styles.fortune}>Get Random Monster</Text>
+      </Pressable>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "grey",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  description: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+  },
+
+  image: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 45,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    fontFamily: "metal-macabre",
+    textShadowColor: "black", // Add text shadow
+    textShadowOffset: { width: 2, height: 2 }, // Adjust the offset as needed
+    textShadowRadius: 5, // Adjust the radius as needed
+  },
+  fortune: {
+    position: "absolute",
+    bottom: 20,
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    backgroundColor: "#8B0000",
+    padding: 10,
+    borderRadius: 10,
+    width: 200,
+    right: -100,
+    textAlign: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
   },
 });
+
+export default App;
---
+++ README.md
@@ -0,0 +1,55 @@
+# D&D Generator App
+
+![App Screenshot](./assets/screenshot.png)
+
+## Description
+
+The D&D Generator App is a simple mobile application built with React Native. Please note that this project is primarily designed as a learning exercise to explore React Native development and is not intended for serious use. It provides a fun way to randomly generate Dungeons & Dragons (D&D) monsters and view their details.
+
+## Features
+
+- Randomly generate D&D monsters.
+- View monster details such as size, type, alignment, challenge rating, hit points, and speed.
+- Learn and practice React Native development concepts.
+- Explore API integration with the D&D 5e API.
+
+## Getting Started
+
+To get started with this project, follow these steps:
+
+1. **Clone the Repository:**
+
+   ```bash
+   git clone https://github.com/stoyanov-kaloyan/dnd-generator-app.git
+   cd dnd-generator-app
+   ```
+
+2. **Install Dependencies:**
+
+   ```bash
+   npm install
+   # or
+   yarn install
+   ```
+
+3. **Run the Application:**
+
+   ```bash
+   expo start
+   ```
+
+4. **View the App:**
+
+   Use the Expo Go app on your mobile device or an emulator/simulator to view and interact with the D&D Generator App.
+
+## Disclaimer
+
+This project is solely for educational purposes and to learn React Native development. The app's data is sourced from the D&D 5e API (https://www.dnd5eapi.co/api/), and it may not provide complete or accurate information for serious D&D gameplay or reference.
+
+## Contributing
+
+Contributions to this project are welcome. Feel free to fork the repository, make improvements, and submit pull requests. If you encounter any issues or have suggestions, please open an issue on the GitHub repository.
+
+## License
+
+This project is open-source and available under the [MIT License](LICENSE).
